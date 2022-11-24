2e9e2141cff500445d1d4e4e12e306de92617d3c
Vladimir Vinokurov <v.vinokyrov@yandex.ru>
2022-11-24 18:40:40 +0300

    add PostViewController. In FeedViewController add button to PostViewController.

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FeedViewController.swift
 //  Navigation
 //
 //  Created by Владимир Винокуров on 23.11.2022.
@@ -9,9 +9,36 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
+    private lazy var button: UIButton = {
+            let button = UIButton()
+            button.backgroundColor = .blue
+            button.layer.cornerRadius = 12
+            button.setTitle("Перейти на пост", for: .normal)
+            button.setTitleColor(.lightGray, for: .normal)
+            button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+            button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            return button
+        }()
+    
+    private func setupButton () {
+            self.view.addSubview(self.button)
+            self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
+            self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+            self.button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+            self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        }
+    
+    @objc private func buttonAction() {
+            let postViewController = PostViewController()
+            self.navigationController?.pushViewController(postViewController, animated: true)
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .cyan
+        setupButton()
+        
         // Do any additional setup after loading the view.
     }
 
--- Navigation/PostViewController.swift
+++ Navigation/PostViewController.swift
@@ -1,17 +1,24 @@
 //
-//  ProfileViewController.swift
+//  PostViewController.swift
 //  Navigation
 //
-//  Created by Владимир Винокуров on 23.11.2022.
+//  Created by Владимир Винокуров on 24.11.2022.
 //
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class PostViewController: UIViewController {
 
+    private func setupView() {
+            self.view.backgroundColor = .lightGray
+            self.navigationItem.title = "Мой пост"
+            self.navigationController?.navigationBar.prefersLargeTitles = true
+            //self.navigationItem.rightBarButtonItem = button
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray
+        setupView()
         // Do any additional setup after loading the view.
     }
     
