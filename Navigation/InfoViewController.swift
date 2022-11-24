e075c0241a2e46d1dcda4c8d1a3c3ae3eb48e534
Vladimir Vinokurov <v.vinokyrov@yandex.ru>
2022-11-24 23:01:17 +0300

    Add ButtonInfo and UIAlertController with two UIAlertActions

--- Navigation/InfoViewController.swift
+++ Navigation/InfoViewController.swift
@@ -12,10 +12,24 @@ class InfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        let buttonInfo = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        buttonInfo.backgroundColor = .systemBlue
+        buttonInfo.setTitle("IKEA покинула нас", for: .normal)
+        buttonInfo.addTarget(self, action: #selector(buttonAlert), for: .touchUpInside)
+        self.view.addSubview(buttonInfo)
 
         // Do any additional setup after loading the view.
     }
-    
+    @objc func buttonAlert () {
+        let alert = UIAlertController(title: "Вы любили покупать товары в IKEA?", message: "выберете ответ", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Да", style: .default, handler: { action in
+            print("Теперь их больше не купить")
+        }))
+        alert.addAction(UIAlertAction(title: "Нет", style: .cancel, handler: { action in
+            print("Значит для вас ничего не изменилось")
+        }))
+        self.present(alert, animated: true)
+    }
 
     /*
     // MARK: - Navigation
