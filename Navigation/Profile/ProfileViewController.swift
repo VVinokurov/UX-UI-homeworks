ef97c86a5c83a5007f6bf23f483bf9967250acec
Vladimir Vinokurov <v.vinokyrov@yandex.ru>
2022-11-29 23:27:19 +0300

    add UI elements in ProfileHeaderView, add @objc func buttonPressed()

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -9,21 +9,27 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    let mainView = ProfileHeaderView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        view.backgroundColor = .white
+        self.view.addSubview(mainView)
+        mainView.addSubview()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillLayoutSubviews() {
+        mainView.frame = CGRect(x: 0, y: 100, width: Int(self.view.frame.width), height: Int(self.view.frame.height))
+        mainView.backgroundColor = .lightGray
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
     }
-    */
-
 }
