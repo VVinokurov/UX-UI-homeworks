65414fa19560a1a173e9cc3d58d5432740687958
Vladimir Vinokurov <v.vinokyrov@yandex.ru>
2022-12-27 14:01:36 +0300

    edits added

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -7,9 +7,6 @@
 
 import UIKit
 
-var cellID = "PostTableViewCell"
-var cellPhotos = "PhotosTableViewCell"
-
 struct Posts {
     var author: String
     var description: String
@@ -24,35 +21,34 @@ class ProfileViewController: UIViewController, UIGestureRecognizerDelegate {
     
     var tableView = UITableView(frame: .zero)
     
-    let box = UIButton(frame: .zero)
-    let blurBox = UIView(frame: .zero)
+    let closeButton = UIButton(frame: .zero)
+    let blackView = UIView(frame: .zero)
     
-    
-    func addBut() {
-        self.view.addSubview(box)
-        box.setImage(UIImage(systemName: "clear"), for: .normal)
-        box.addTarget(self, action: #selector(tapReverse), for: .touchUpInside)
-        box.translatesAutoresizingMaskIntoConstraints = false
-        box.backgroundColor = .clear
-        box.alpha = 0.0
+    func addButton() {
+        self.view.addSubview(closeButton)
+        closeButton.setImage(UIImage(systemName: "clear"), for: .normal)
+        closeButton.addTarget(self, action: #selector(tapReverse), for: .touchUpInside)
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.backgroundColor = .clear
+        closeButton.alpha = 0.0
         NSLayoutConstraint.activate([
-            box.trailingAnchor.constraint(equalTo: tableView.trailingAnchor, constant: -16),
-            box.topAnchor.constraint(equalTo: tableView.topAnchor, constant: 16),
-            box.heightAnchor.constraint(equalToConstant: 20),
-            box.widthAnchor.constraint(equalToConstant: 20)
+            closeButton.trailingAnchor.constraint(equalTo: tableView.trailingAnchor, constant: -16),
+            closeButton.topAnchor.constraint(equalTo: tableView.topAnchor, constant: 16),
+            closeButton.heightAnchor.constraint(equalToConstant: 20),
+            closeButton.widthAnchor.constraint(equalToConstant: 20)
         ])
     }
     
-    func blur() {
-        self.view.addSubview(blurBox)
-        blurBox.translatesAutoresizingMaskIntoConstraints = false
-        blurBox.backgroundColor = .white
-        blurBox.alpha = 0.0
+    func addBlackView() {
+        self.view.addSubview(blackView)
+        blackView.translatesAutoresizingMaskIntoConstraints = false
+        blackView.backgroundColor = .white
+        blackView.alpha = 0.0
         NSLayoutConstraint.activate([
-            blurBox.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            blurBox.topAnchor.constraint(equalTo: view.topAnchor),
-            blurBox.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            blurBox.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+            blackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            blackView.topAnchor.constraint(equalTo: view.topAnchor),
+            blackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            blackView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
         ])
     }
     
@@ -60,21 +56,20 @@ class ProfileViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @objc func tapReverse () {
         UIView.animate(withDuration: 0.3, delay: 0, animations: {
-            self.box.alpha = 0.0
+            self.closeButton.alpha = 0.0
         }) { _ in
             UIView.animate(withDuration: 0.5, delay: 0, animations: {
-                self.blurBox.alpha = 0.0
+                self.blackView.alpha = 0.0
                 self.profileView.avatar.layer.cornerRadius = 64.0
                 self.profileView.avatar.layer.borderWidth = 3.0
                 self.profileView.avatar.center.x = 80
                 self.profileView.avatar.center.y = 80
-                self.profileView.avatar.transform = CGAffineTransform.identity
+                self.profileView.avatar.layer.bounds = CGRect (x: 0, y: 0, width: 128, height: 128)
             })
         }
     }
     
     let profileView = ProfileHeaderView()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -84,8 +79,8 @@ class ProfileViewController: UIViewController, UIGestureRecognizerDelegate {
         self.tableView.delegate = self
         self.tableView.rowHeight = UITableView.automaticDimension
         self.tableView.sectionHeaderTopPadding = .leastNormalMagnitude
-        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: cellID)
-        tableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: cellPhotos)
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "PostTableViewCell")
+        tableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: "PhotosTableViewCell")
         tableView.translatesAutoresizingMaskIntoConstraints = false
         profileView.addSubview()
         profileView.setupConstraints()
@@ -93,7 +88,6 @@ class ProfileViewController: UIViewController, UIGestureRecognizerDelegate {
         self.tableView.layer.opacity = 1.0
         profileView.avatar.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap)))
         self.profileView.bringSubviewToFront(profileView.avatar)
-        
        
         
         let safeArea = self.view.safeAreaLayoutGuide
@@ -106,23 +100,27 @@ class ProfileViewController: UIViewController, UIGestureRecognizerDelegate {
     
     }
     
-    @objc func tap(_ sender: UITapGestureRecognizer){
-        self.blurBox.bringSubviewToFront(self.profileView.avatar)
-        self.profileView.avatar.superview?.bringSubviewToFront(self.profileView.avatar)
-        self.blur()
-        self.addBut()
+    @objc func tap(){
+        //self.profileView.avatar.superview?.bringSubviewToFront(self.profileView.avatar)
+        self.addBlackView()
+        self.addButton()
+        //self.blackView.bringSubviewToFront(self.profileView.avatar)
     
-        UIView.animate(withDuration: 0.5, delay: 0, animations: {
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+            self.blackView.alpha = 0.5
             self.profileView.avatar.layer.cornerRadius = 0.0
-            self.profileView.avatar.layer.borderWidth = 2.0
-            self.profileView.avatar.center.x = self.tableView.bounds.width/2
-            self.profileView.avatar.center.y = self.tableView.bounds.height/2
-            let width = self.view.bounds.width / self.profileView.avatar.bounds.width
-            self.profileView.avatar.transform = CGAffineTransform(scaleX: width, y: width)
-            self.blurBox.alpha = 0.5
+            self.profileView.avatar.layer.borderWidth = 0.0
+            self.profileView.avatar.layer.bounds = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width,
+            height: UIScreen.main.bounds.width)
+            self.profileView.avatar.center = CGPoint(x: self.tableView.bounds.width/2, y: self.tableView.bounds.height/2)
+            self.profileView.avatar.layoutIfNeeded()
+//            self.profileView.avatar.center.x = self.tableView.bounds.width/2
+//            self.profileView.avatar.center.y = self.tableView.bounds.height/2
+//            let width = self.view.bounds.width / self.profileView.avatar.bounds.width
+//            self.profileView.avatar.transform = CGAffineTransform(scaleX: width, y: width)
         }) { _ in
             UIView.animate(withDuration: 0.3, delay: 0, animations: {
-                self.box.alpha = 1.0
+                self.closeButton.alpha = 1.0
             })
         }
     }
@@ -163,10 +161,10 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch(indexPath.section) {
         case 0:
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellPhotos, for: indexPath) as! PhotosTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PhotosTableViewCell", for: indexPath) as! PhotosTableViewCell
             return cell
         default:
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! PostTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell", for: indexPath) as! PostTableViewCell
             cell.author.text = postsForTable[indexPath.item].author
             cell.image.image = UIImage(named: postsForTable[indexPath.item].image)
             cell.descrip.text = postsForTable[indexPath.item].description
